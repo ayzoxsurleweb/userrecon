a1865f23f11060941c68d6e0a1061194ce640f67
ayzoxsurleweb <144566739+ayzoxsurleweb@users.noreply.github.com>
2023-09-10 01:05:37 +0200

    Add files via upload

--- userrecon.sh
+++ userrecon.sh
@@ -1038,15 +1038,15 @@ fi
 
 ##tiktok
 
-printf "\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] : \e[0m"
-check1=$(curl -s -i "https://www.tiktok.com/$username" -H "Accept-Language: en" -L | grep -o 'location: https://www.tiktok.com/'$username ; echo $?)
+printf "\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] tiktok: \e[0m"
+check1=$(curl -s -i "https://www.tiktok.com/$username" -H "Accept-Language: en" -L | grep -o 'location: https://www.tiktok.com/'$username'' ; echo $?)
 
 if [[ $check1 == *'0'* ]] ; then 
 printf "\e[1;93mNot Found!\e[0m\n"
 elif [[ $check1 == *'1'* ]]; then 
 
-printf "\e[1;92m Found!\e[0m https://www.tiktok.com/" $username
-printf "https://www.tiktok.com/" $username >> $username.txt
+printf "\e[1;92m Found!\e[0m https://www.tiktok.com/$username'
+printf "https://www.tiktok.com/$username" >> $username.txt
 fi
 
 partial
